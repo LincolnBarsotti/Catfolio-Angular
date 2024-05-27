6da25c22d56a56ee61596579e2f585892aefc8bc
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2024-05-26 22:47:09 -0300

    :bricks: build: backend

--- catfolio/src/app/components/login/login.component.ts
+++ catfolio/src/app/components/login/login.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   login:string = "";
   senha:string = "";
+
+  logar(){
+    console.log(this.login + " " + this.senha);
+  }  
 
 }
